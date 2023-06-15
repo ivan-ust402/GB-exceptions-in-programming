6c5fe833f4d1b1a8e632b6ccb5b21eb183de9750
Ivan <Vangold915@gmail.com>
2023-06-15 18:49:16 +0700

    Completed task2

--- homework1/task2.java
+++ homework1/task2.java
@@ -6,9 +6,9 @@ package homeworks.homework1;
  * public static int sum2d(String[][] arr){ 
  *  int sum = 0; 
  *  for (int i = 0; i < arr.length; i++) { 
- *  for (int j = 0; j < 5; j++) { 
- *      int val = Integer.parseInt(arr[i][j]); 
- *      sum += val; 
+ *      for (int j = 0; j < 5; j++) { 
+ *          int val = Integer.parseInt(arr[i][j]); 
+ *          sum += val; 
  *      } 
  *  } 
  *  return sum; 
@@ -16,6 +16,22 @@ package homeworks.homework1;
  */
 public class task2 {
     public static void main(String[] args) {
-        
+        String[][] array = new String[][]{{"1", "2", "3", "4", "5"},{"4", "5", "6", "7", "7"},{"7", "8", "9", "0", "0"}};
+        System.out.println(sum2d(array));
     }
+
+    public static int sum2d(String[][] arr){ 
+        int sum = 0; 
+        for (int i = 0; i < arr.length; i++) { 
+            for (int j = 0; j < 5; j++) { 
+                int val = Integer.parseInt(arr[i][j]); 
+                sum += val; 
+            } 
+        } 
+        return sum; 
+    }
+
+    // Ответ:
+    // 1. ArrayIndexOfBoundsException
+    // 2. NumberFormatException
 }
