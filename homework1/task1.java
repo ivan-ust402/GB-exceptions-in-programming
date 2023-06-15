bec20dd60f807c9f3f515fc7d193d8a106117ced
Ivan <Vangold915@gmail.com>
2023-06-15 17:44:28 +0700

    Homework1: Initial commit

---
+++ homework1/task1.java
@@ -0,0 +1,13 @@
+package homeworks.homework1;
+
+/**
+ * Задача 1
+ * Реализуйте 3 метода, чтобы в каждом из них получить разные исключения
+ */
+
+public class task1 {
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ homework1/task2.java
@@ -0,0 +1,21 @@
+package homeworks.homework1;
+
+/**
+ * Задача 2
+ * Посмотрите на код, и подумайте сколько разных типов исключений вы тут сможете получить? 
+ * public static int sum2d(String[][] arr){ 
+ *  int sum = 0; 
+ *  for (int i = 0; i < arr.length; i++) { 
+ *  for (int j = 0; j < 5; j++) { 
+ *      int val = Integer.parseInt(arr[i][j]); 
+ *      sum += val; 
+ *      } 
+ *  } 
+ *  return sum; 
+ * }
+ */
+public class task2 {
+    public static void main(String[] args) {
+        
+    }
+}
