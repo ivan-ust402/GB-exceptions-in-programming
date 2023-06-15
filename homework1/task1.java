61a6438c10aaaafe2c340cb8ee4719a700ea391e
Ivan <Vangold915@gmail.com>
2023-06-15 17:51:07 +0700

    created ArithmeticException example

--- homework1/task1.java
+++ homework1/task1.java
@@ -7,7 +7,14 @@ package homeworks.homework1;
 
 public class task1 {
     public static void main(String[] args) {
-        
+        // 1 случай деление на ноль, ArithmeticException
+        // System.out.println(divide(10, 0));
     }
 
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("На ноль делить нельзя!");
+        }
+        return a/b;
+    }
 }
