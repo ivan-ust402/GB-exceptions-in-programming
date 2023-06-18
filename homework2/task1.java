69bc4ed17f67d9fb87273c739a09e4c2d3181980
Ivan <Vangold915@gmail.com>
2023-06-18 11:31:34 +0700

    Completed task 1

--- homework2/task1.java
+++ homework2/task1.java
@@ -1,5 +1,7 @@
 package homeworks.homework2;
 
+import java.util.Scanner;
+
 public class task1 {
     // Задание 1
     // Реализуйте метод, который запрашивает у пользователя ввод дробного числа 
@@ -7,6 +9,36 @@ public class task1 {
     // не должно приводить к падению приложения, вместо этого, необходимо 
     // повторно запросить у пользователя ввод данных.
     public static void main(String[] args) {
-        
+        UIReturnEnteringNumber();
+    }
+
+    public static void UIReturnEnteringNumber() {
+        Scanner scan = new Scanner(System.in);
+        boolean condition = true;
+        int counter = 0;
+        while (condition) {
+            System.out.print("Введите дробное число: ");
+            String inputString = scan.next();
+            try {
+                float floatNumber = Float.parseFloat(inputString);
+                System.out.println(returnEnteringNumber(floatNumber));
+            } catch (NumberFormatException e) {
+                System.out.println("Вы ввели не число!");
+            }
+            
+            if (counter % 5 == 0 && counter != 0) {
+                System.out.println("Для продолжения введите любую букву. Для выхода введите 'q': ");
+                String exit = scan.next();
+                if (exit.equals("q")) {
+                    condition = false;                
+                }
+            }
+            counter++;
+        }
+        scan.close();
+    }
+
+    public static float returnEnteringNumber(float number) {
+        return number;
     }
 }
