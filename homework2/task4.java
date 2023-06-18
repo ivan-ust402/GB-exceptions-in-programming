f2232739c35709aa666ac30706e3beb93ccb2a73
Ivan <Vangold915@gmail.com>
2023-06-18 12:25:14 +0700

    Completed task 4

--- homework2/task4.java
+++ homework2/task4.java
@@ -1,11 +1,31 @@
 package homeworks.homework2;
 
+import java.util.Scanner;
+
 public class task4 {
     // Задание 4
     // Разработайте программу, которая выбросит Exception, когда пользователь 
     // вводит пустую строку. Пользователю должно показаться сообщение, что 
     // пустые строки вводить нельзя.
     public static void main(String[] args) {
-        
+        System.out.println(emptyString()); 
+    }
+
+    public static String emptyString() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите ваше сообщение: ");
+        String inputString = "";
+        try {
+            inputString = scan.nextLine();  
+            if (inputString.isEmpty()){
+                inputString = "Вы ничего не написали!";
+                throw new IllegalArgumentException("Строка пустая");
+            }          
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        } finally {
+            scan.close();            
+        }
+        return "Ваше сообщение: " + inputString;
     }
 }
