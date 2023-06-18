1e9859e93aa3200b96e16cc31b7bfad5286470f1
Ivan <Vangold915@gmail.com>
2023-06-18 10:56:41 +0700

    Homework2: Initial commit

---
+++ homework2/task2.java
@@ -0,0 +1,10 @@
+package homeworks.homework2;
+
+public class task2 {
+    // Задание 2
+    // Если необходимо, исправьте данный код 
+    // (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ homework2/task3.java
@@ -0,0 +1,10 @@
+package homeworks.homework2;
+
+public class task3 {
+    // Задание 3
+    // Дан следующий код, исправьте его там, где требуется 
+    // (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+    public static void main(String[] args) {
+        
+    }
+}
